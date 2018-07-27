6b9e5fff4c801db8d80e8aec1e9b663080304360
GreenPiggies <hungwesley@gmail.com>
2018-07-27 08:18:55 -0700

    :D

--- Platformer/KinematicBody2D.gd
+++ Platformer/KinematicBody2D.gd
@@ -10,18 +10,21 @@ var motion = Vector2()
 func _physics_process(delta):
 	motion.y += GRAVITY
 	var friction = false
+	motion.x = MAX_SPEED
+	$Sprite.flip_h = false
+	$Sprite.play("Run")
 	
-	if Input.is_action_pressed("ui_right"):
-		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
-		$Sprite.flip_h = false
-		$Sprite.play("Run")
-	elif Input.is_action_pressed("ui_left"):
-		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
-		$Sprite.flip_h = true
-		$Sprite.play("Run")
-	else:
-		$Sprite.play("Idle")
-		friction = true
+	#if Input.is_action_pressed("ui_right"):
+	#	motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
+	#	$Sprite.flip_h = false
+	#	$Sprite.play("Run")
+	#elif Input.is_action_pressed("ui_left"):
+	#	motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
+	#	$Sprite.flip_h = true
+	#	$Sprite.play("Run")
+	#else:
+	#	$Sprite.play("Idle")
+	#	friction = true
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
@@ -34,6 +37,8 @@ func _physics_process(delta):
 			$Sprite.play("Fall")
 		if friction == true:
 			motion.x = lerp(motion.x, 0, 0.05)
+			
+	
 		
 	motion = move_and_slide(motion, UP)
 	print(motion)
